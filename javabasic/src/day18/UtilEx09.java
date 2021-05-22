51d08e45ce8ad3459059d9f5766640e713f9e6b7
dandiico <dandiico@daum.net>
2021-05-22 21:03:15 +0900

    Fix input, quiz method
    - input method는 Utilex08과 같은 이유로 수정 하였고, HaspMap Class에 containsKey라는 아주
    좋은 method가 있어서 간결하게 코드 수정
    - quiz method의 경우 나라이름이 랜덤으로 나올 수 있도록 하는 코드는 금방 작성하였는데, 정답을 맞춰도 아니라는
    메세지가 나와서 뭐지? 싶다가 key에 해당하는 value와 비교해야 하는걸 key와 동일한지 비교해버렸음. ArrayList와
    혼돈하여서 그랬던듯 생각하다 깨닫고 수정 완료

--- javabasic/src/day18/UtilEx09.java
+++ javabasic/src/day18/UtilEx09.java
@@ -1,6 +1,5 @@
 package day18;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -16,24 +15,9 @@ import java.util.Set;
 class CapitalGame2{
 	HashMap<String, String> map = new HashMap<String,String>();
 	Scanner sc = new Scanner(System.in);
-	// 뽑아 놓을 key들 담아놓을 list 만들기
-	ArrayList<String> list = new ArrayList<String>();
 	
-	// key들만 뽑아서 모아놓기
-		void keys() {
-			Set<String> keys = map.keySet();
-			Iterator<String> it = keys.iterator();
-			// key들 전체 꺼내서 list에 담기
-			while(it.hasNext()) {
-				String key = it.next();
-				list.add(key);
-			}
-		}
-		
-	}
-				
 	void setting() {
-		map.put("그리스","아테네");
+		map.put("스페인","마드리드");
 		map.put("중국","베이징");
 		map.put("러시아","모스크바");
 		map.put("독일","베를린");
@@ -44,31 +28,70 @@ class CapitalGame2{
 	}
 	
 	void input() {
-		System.out.println("현재" + map.size() + "나라와 수도가 입력되어 있습니다.");
+		System.out.println("현재 " + map.size() + "개의 나라와 수도가 입력되어 있습니다.");
+		
 		while(true) {
-			System.out.println("나라와 수도 입력" + map.size()+1);
+			System.out.println("나라와 수도 입력" + (map.size()+1) + ">>");
 			String str = sc.nextLine();
-			// 입력받은 문자열 나눠서 배열에 담아넣기
 			String[] arr = str.split(" ");
 			
-			for(int i = 0; i<list.size(); i++) {
-				if(str.equals("그만")) {
-					break;
-				}else if(arr[0].equals(list.get(i))) {
-					System.out.println(arr[0] + "은 이미 있습니다!!");
-				} map.put(arr[0], arr[i]);
+			if(str.equals("그만")) {
+				break;
+			}
+			if(map.containsKey(arr[0])) {
+				System.out.println(arr[0] + "는 이미 있습니다.");
+			}else {
+				map.put(arr[0], arr[1]);
 			}
 			
 		}
 	}
-	void quiz() {}
+	void quiz() {
+		// key 전부 뽑아서 배열에 넣어두고, 랜덤 번호 돌려서 나오도록 하기
+		Set<String> keys = map.keySet();
+		Iterator<String> it = keys.iterator();
+		String [] keysArr = new String [map.size()];
+		int i = 0;
+		while(it.hasNext()) {
+			keysArr[i] = it.next();
+			i++;
+		}
+		int [] randomArr = new int [keysArr.length];
+		for(int j = 0; j<keysArr.length; j++) {
+			randomArr[j] = (int)(Math.random()*keysArr.length);
+			// 번호 중복인지 아닌지 확인후 중복일시 다시 뽑기
+			for(int k = 0; k<j; k++) {
+				if(randomArr[j] == randomArr[k]) {
+					j--;
+				}
+			}
+		}
+		for(int t : randomArr) 
+		{System.out.println(t);}
+		for(String t : keysArr) 
+		{System.out.println(t);}
+		
+		for(int n = 0; n<keysArr.length; n++) {
+			System.out.println(keysArr[randomArr[n]] + "의 수도는?");
+			String str = sc.nextLine();
+			if(str.equals("그만")){
+				break;
+			}else if(map.get(keysArr[randomArr[n]]).equals(str)) {
+				System.out.println("정답!!");
+			} else {
+				System.out.println("아닙니다!!");
+			}
+		}
+		System.out.println("문제가 끝났습니다");
+	}
 	
 	void run() {
 		System.out.println("**수도 맞추기 게임을 시작합니다**");
 		while(true) {
 			setting();
 			System.out.println("입력:1, 퀴즈:2, 종료:3 >> ");
-			int sel = sc.nextInt();
+			int sel = Integer.parseInt(sc.nextLine());
+			
 			if(sel == 1) {
 				input();
 			}else if(sel == 2) {
@@ -83,9 +106,9 @@ class CapitalGame2{
 }
 public class UtilEx09 {
 	public static void main(String[] args) {
-		
+		CapitalGame2 c = new CapitalGame2();
 		CapitalGame2 game = new CapitalGame2();
-		game.run();
-		
+		//game.run();
+		c.run();
 	}
 }
